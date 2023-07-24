9867c9725bb95ea6bdec4c49c3e1c181e8c3ebce
AzaziL <121525098+MolCoteH@users.noreply.github.com>
2023-07-24 18:21:34 +0330

    Update Modular_inverse_Solver.py

--- Modular_inverse_Solver.py
+++ Modular_inverse_Solver.py
@@ -3,39 +3,36 @@ import math
 alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789_'
 uppercase_alphabet = alphabet.upper()
 
-message = input("Enter your message as a list of numbers: ")
-message = [int(num) for num in message.split()]
-
-mod = int(input("Enter the modulus: "))
-
+while True:
+  message = input("Enter your message as a list of numbers: ")
+  try:
+    message = [int(num) for num in message.split()]
+    break
+  except ValueError:
+    print("Please enter only numbers separated by spaces")
+    
+while True:
+  mod = input("Enter the modulus: ")
+  if mod.isdigit():
+    mod = int(mod)
+    break 
+  else:
+    print("Please enter a number")
+    
 uppercase = input("Use uppercase letters? (y/n): ").lower() == 'y'
 if uppercase:
   alphabet = uppercase_alphabet
+  
+while True:
+  from_num = input("Start modular inverse from (leave blank for default, 0 for normal): ")
+  if from_num.isdigit() or from_num == '' or from_num == '0':
+    break
+  else:
+    print("Please enter a number or leave blank")
+    
+if from_num == '0' or from_num == '':
+  message = [str(num) for num in message]
 
-print("Choose decryption method:")
-print("1. Modular inverse(normal-start)")
-print("2. Modular inverse(-1)") 
-
-choice = input("Enter 1 or 2: ")
-
-if choice == '2':
-
-  print('\n')
-  result = ''
-  for num in message:
-      mod_inv = pow(num % mod, -1, mod)
-      index = mod_inv - 1
-      print(index,end=' ')  
-      char = alphabet[index]
-      result += char
-
-  print('\n',result)
-
-elif choice == '1':
-
-  message = [str(num) for num in message] # Convert back to strings
-
-  # Dynamically generate character mapping based on modulus
   char_map = {}
   for i in range(mod):
     char_map[i] = alphabet[i % len(alphabet)]
@@ -44,10 +41,24 @@ elif choice == '1':
   print('\n')
   for num in message:
     num = int(num) % mod
-    print(num,end=' ')
+    print(num, end=' ')
     decrypted += char_map[num]
 
-  print('\n',decrypted)
+  print('\n', decrypted)
+
+elif from_num:
+  from_num = int(from_num)
   
+  print('\n')
+  result = ''
+  for num in message:
+    mod_inv = pow(num % mod, from_num, mod)
+    index = (mod_inv + from_num) % len(alphabet)
+    print(index, end=' ')
+    char = alphabet[index]
+    result += char
+
+  print('\n', result)
+
 else:
-  print("Invalid choice")
+   print('Goodby')
